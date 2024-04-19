cedec1578ed179b97af86508225d6001ceb9de84
anubha <anubha.rajput@innotechteam.in>
2024-04-19 17:42:30 +0530

    add css

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ export default {
   data() {
     return {
       user: {
-        userName: 'Anubha Rajput',
+        name: 'Anubha Rajput',
         address: '123 Main Street',
         dob: '2000-10-21'
       },
@@ -12,10 +12,10 @@ export default {
       isError: null,
       isValid:false,
       users: [
-        { name: 'Rahul', age: 24, imageUrl: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/35af6a41332353.57a1ce913e889.jpg" },
-        { name: 'Rohit', age: 20, imageUrl: "https://s3-eu-west-1.amazonaws.com/files2.fd.nl/Erwin/Slider+Daan/stefan-bron-2.jpg" },
-        { name: 'Anubha', age: 21, imageUrl: "https://static.nieuwsblad.be/Assets/Images_Upload/2012/10/07/Stefan-004-kl.jpg" },
-        { name: 'amit', age: 21, imageUrl: "https://media.licdn.com/dms/image/C4E03AQGAHt1PVFJO6A/profile-displayphoto-shrink_800_800/0/1623249411702?e=2147483647&v=beta&t=PhjGA8rK15XMw5cFPTLz96KWHaYP_beHpHfqgoHj7bM" }
+        { userName: 'Rahul', age: 24, imageUrl: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/35af6a41332353.57a1ce913e889.jpg" },
+        { userName: 'Rohit', age: 20, imageUrl: "https://s3-eu-west-1.amazonaws.com/files2.fd.nl/Erwin/Slider+Daan/stefan-bron-2.jpg" },
+        { userName: 'Anubha', age: 21, imageUrl: "https://static.nieuwsblad.be/Assets/Images_Upload/2012/10/07/Stefan-004-kl.jpg" },
+        { userName: 'amit', age: 21, imageUrl: "https://media.licdn.com/dms/image/C4E03AQGAHt1PVFJO6A/profile-displayphoto-shrink_800_800/0/1623249411702?e=2147483647&v=beta&t=PhjGA8rK15XMw5cFPTLz96KWHaYP_beHpHfqgoHj7bM" }
       ],
       validated: true,
     };
